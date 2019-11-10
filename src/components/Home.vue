b623350dc4300bfcf52ab3246d6067bc4b19e137
Valentin U <valentin@kangaroorewards.com>
2019-11-09 21:21:48 -0500

    change logo

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3></h3>
+
+    <ul>
+      <li v-for="item in items"  :key="item.id" :item="item">
+        <a :href="item.link">{{ item.text }}</a>
+      </li>
+    </ul>
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+    item: Object
+  },
+  data: function(){
+    return {
+      items: []
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  mounted: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      this.items = [
+          {id: 1, link: 'https://www.burnfatnotsugar.com/', text: 'BURN FAT NOT SUGAR' },
+          {id: 2, link: 'https://thepediet.com/', text: 'The P:E Diet' }
+        ]
+      // var xhr = new XMLHttpRequest()
+      // var self = this
+      // xhr.open('GET', apiURL + self.currentBranch)
+      // xhr.onload = function () {
+      //   self.commits = JSON.parse(xhr.responseText)
+      //   console.log(self.commits[0].html_url)
+      // }
+      // xhr.send()
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+// }
+a {
+  color: #42b983;
+}
+</style>
